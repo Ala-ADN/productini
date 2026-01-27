41277559ac36be3d066de8b1565e510b237afec2
Dali Guerfali <daliguerfali@gmail.com>
2026-01-27 13:17:17 +0000

    added habit counter layout template + styling

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HabitCounter } from './components/habit-counter/habit-counter';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HabitCounter],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('angular-app');
+  protected readonly title = signal('Prodhub');
 }
