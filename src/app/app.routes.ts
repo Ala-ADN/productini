80045091f3bc81258f054971d1bd3e417782a459
rayenbbs <rayenbesbesbesbes@gmail.com>
2026-01-29 15:43:43 +0000

    feat: dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: DashboardComponent },
+    { path: 'todo', component: TodoListComponent },
+];
